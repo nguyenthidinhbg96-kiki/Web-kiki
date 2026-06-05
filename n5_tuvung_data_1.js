babdade030afe2c26eccff014612773a5d0bd8e2
nguyenthidinhbg96-kiki <nguyenthidinhbg96@gmail.com>
2026-06-05 21:48:50 +0900

    Add files via upload

---
+++ n5_tuvung_data_1.js
@@ -0,0 +1,1002 @@
+window.n5_tuvung_data_1 = [
+
+{
+level:"N5",
+lesson:1,
+id:1,
+
+kanji:"私",
+kana:"わたし",
+hanviet:"TƯ",
+vi:"Tôi",
+en:"I",
+category:"Minna 1",
+example:`
+<span class="target-word">
+<ruby>
+私<rt>わたし</rt>
+</ruby>
+</span>は
+<ruby>学生<rt>がくせい</rt></ruby>です。
+`,
+example_read:"わたしは がくせいです。",
+example_vi:"Tôi là học sinh.",
+example_en:"I am a student."
+},
+
+{
+level:"N5",
+lesson:1,
+id:2,
+
+kanji:"",
+kana:"あなた",
+hanviet:"",
+vi:"Anh, chị, ông, bà",
+en:"You",
+category:"Minna 1",
+
+example:`
+<span class="target-word">あなた</span>は
+<ruby>先生<rt>せんせい</rt></ruby>ですか。
+`,
+example_read:"あなたは せんせいですか。",
+example_vi:"Anh/chị là giáo viên phải không?",
+example_en:"Are you a teacher?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:3,
+
+kanji:"あの人",
+kana:"あのひと",
+hanviet:"NHÂN",
+vi:"Người kia, người đó",
+en:"That person",
+category:"Minna 1",
+
+example:`
+<span class="target-word">
+あの<ruby>人<rt>ひと</rt></ruby>
+</span>は<ruby>日本人<rt>にほんじん</rt></ruby>です。
+`,
+example_read:"あのひとは にほんじんです。",
+example_vi:"Người đó là người Nhật.",
+example_en:"That person is Japanese."
+},
+
+{
+level:"N5",
+lesson:1,
+id:4,
+
+kanji:"あの方",
+kana:"あのかた",
+hanviet:"PHƯƠNG",
+vi:"Vị kia (lịch sự)",
+en:"That person (polite)",
+category:"Minna 1",
+
+example:`
+<span class="target-word">
+あの<ruby>方<rt>かた</rt></ruby></span>は<ruby>先生<rt>せんせい</rt></ruby>です。
+`,
+example_read:"あのかたは せんせいです。",
+example_vi:"Vị kia là giáo viên.",
+example_en:"That person is a teacher."
+},
+
+{
+level:"N5",
+lesson:1,
+id:5,
+
+kanji:"～さん",
+kana:"～さん",
+hanviet:"",
+vi:"Anh, chị, ông, bà",
+en:"Mr., Mrs., Ms.",
+category:"Minna 1",
+
+example:`
+<ruby>田中<rt>たなか</rt></ruby><span class="target-word">さん</span>は
+<ruby>会社員<rt>かいしゃいん</rt></ruby>です。
+`,
+example_read:"たなかさんは かいしゃいんです。",
+example_vi:"Anh Tanaka là nhân viên công ty.",
+example_en:"Mr. Tanaka is a company employee."
+},
+
+{
+level:"N5",
+lesson:1,
+id:6,
+
+kanji:"～ちゃん",
+kana:"～ちゃん",
+hanviet:"",
+vi:"Hậu tố gọi trẻ em thân mật",
+en:"Suffix for children",
+category:"Minna 1",
+
+example:`
+<ruby>さくら</ruby>
+<span class="target-word">ちゃん</span>は
+<ruby>五歳<rt>ごさい</rt></ruby>です。
+`,
+example_read:"さくらちゃんは ごさいです。",
+example_vi:"Bé Sakura 5 tuổi.",
+example_en:"Sakura-chan is five years old."
+},
+
+{
+level:"N5",
+lesson:1,
+id:7,
+
+kanji:"～人",
+kana:"～じん",
+hanviet:"NHÂN",
+vi:"Người nước ~",
+en:"Nationality suffix",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+ベトナム<span class="target-word">
+<ruby>人<rt>じん</rt></ruby></span>です。
+`,
+example_read:"わたしは ベトナムじんです。",
+example_vi:"Tôi là người Việt Nam.",
+example_en:"I am Vietnamese."
+},
+
+{
+level:"N5",
+lesson:1,
+id:8,
+
+kanji:"先生",
+kana:"せんせい",
+hanviet:"TIÊN SINH",
+vi:"Thầy giáo, cô giáo",
+en:"Teacher",
+category:"Minna 1",
+
+example:`
+<ruby>山田<rt>やまだ</rt></ruby><span class="target-word"><ruby>先生<rt>せんせい</rt></ruby></span>は
+<ruby>親切<rt>しんせつ</rt></ruby>です。
+`,
+example_read:"やまだせんせいは しんせつです。",
+example_vi:"Thầy Yamada rất tốt bụng.",
+example_en:"Mr. Yamada is kind."
+},
+
+{
+level:"N5",
+lesson:1,
+id:9,
+
+kanji:"教師",
+kana:"きょうし",
+hanviet:"GIÁO SƯ",
+vi:"Giáo viên",
+en:"Teacher",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<ruby>日本語<rt>にほんご</rt></ruby>の
+<span class="target-word"><ruby>教師<rt>きょうし</rt></ruby></span>です。
+`,
+example_read:"わたしは にほんごの きょうしです。",
+example_vi:"Tôi là giáo viên tiếng Nhật.",
+example_en:"I am a Japanese teacher."
+},
+
+{
+level:"N5",
+lesson:1,
+id:10,
+
+kanji:"学生",
+kana:"がくせい",
+hanviet:"HỌC SINH",
+vi:"Học sinh, sinh viên",
+en:"Student",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word"><ruby>学生<rt>がくせい</rt></ruby></span>です。
+`,
+example_read:"わたしは がくせいです。",
+example_vi:"Tôi là sinh viên.",
+example_en:"I am a student."
+},
+{
+level:"N5",
+lesson:1,
+id:11,
+
+kanji:"会社員",
+kana:"かいしゃいん",
+hanviet:"HỘI XÃ VIÊN",
+vi:"Nhân viên công ty",
+en:"Company employee",
+category:"Minna 1",
+
+example:`
+<ruby>父<rt>ちち</rt></ruby>は
+<span class="target-word"><ruby>会社員<rt>かいしゃいん</rt></ruby></span>です。
+`,
+example_read:"ちちは かいしゃいんです。",
+example_vi:"Bố tôi là nhân viên công ty.",
+example_en:"My father is a company employee."
+},
+
+{
+level:"N5",
+lesson:1,
+id:12,
+
+kanji:"社員",
+kana:"しゃいん",
+hanviet:"XÃ VIÊN",
+vi:"Nhân viên",
+en:"Employee",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+IMCの<span class="target-word"><ruby>社員<rt>しゃいん</rt></ruby></span>です。
+`,
+example_read:"わたしは IMCの しゃいんです。",
+example_vi:"Tôi là nhân viên của IMC.",
+example_en:"I am an employee of IMC."
+},
+
+{
+level:"N5",
+lesson:1,
+id:13,
+
+kanji:"銀行員",
+kana:"ぎんこういん",
+hanviet:"NGÂN HÀNG VIÊN",
+vi:"Nhân viên ngân hàng",
+en:"Bank employee",
+category:"Minna 1",
+
+example:`
+<ruby>兄<rt>あに</rt></ruby>は
+<span class="target-word"><ruby>銀行員<rt>ぎんこういん</rt></ruby></span>です。
+`,
+example_read:"あには ぎんこういんです。",
+example_vi:"Anh trai tôi là nhân viên ngân hàng.",
+example_en:"My older brother is a bank employee."
+},
+
+{
+level:"N5",
+lesson:1,
+id:14,
+
+kanji:"医者",
+kana:"いしゃ",
+hanviet:"Y GIẢ",
+vi:"Bác sĩ",
+en:"Doctor",
+category:"Minna 1",
+
+example:`
+<ruby>母<rt>はは</rt></ruby>は
+<span class="target-word"><ruby>医者<rt>いしゃ</rt></ruby></span>です。
+`,
+example_read:"ははは いしゃです。",
+example_vi:"Mẹ tôi là bác sĩ.",
+example_en:"My mother is a doctor."
+},
+
+{
+level:"N5",
+lesson:1,
+id:15,
+
+kanji:"研究者",
+kana:"けんきゅうしゃ",
+hanviet:"NGHIÊN CỨU GIẢ",
+vi:"Nhà nghiên cứu",
+en:"Researcher",
+category:"Minna 1",
+
+example:`
+<ruby>田中<rt>たなか</rt></ruby>さんは
+<span class="target-word"><ruby>研究者<rt>けんきゅうしゃ</rt></ruby></span>です。
+`,
+example_read:"たなかさんは けんきゅうしゃです。",
+example_vi:"Anh Tanaka là nhà nghiên cứu.",
+example_en:"Mr. Tanaka is a researcher."
+},
+
+{
+level:"N5",
+lesson:1,
+id:16,
+
+kanji:"大学",
+kana:"だいがく",
+hanviet:"ĐẠI HỌC",
+vi:"Trường đại học",
+en:"University",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word"><ruby>大学<rt>だいがく</rt></ruby></span>の
+<ruby>学生<rt>がくせい</rt></ruby>です。
+`,
+example_read:"わたしは だいがくの がくせいです。",
+example_vi:"Tôi là sinh viên đại học.",
+example_en:"I am a university student."
+},
+
+{
+level:"N5",
+lesson:1,
+id:17,
+
+kanji:"病院",
+kana:"びょういん",
+hanviet:"BỆNH VIỆN",
+vi:"Bệnh viện",
+en:"Hospital",
+category:"Minna 1",
+
+example:`
+<span class="target-word"><ruby>病院<rt>びょういん</rt></ruby></span>は
+どこですか。
+`,
+example_read:"びょういんは どこですか。",
+example_vi:"Bệnh viện ở đâu?",
+example_en:"Where is the hospital?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:18,
+
+kanji:"だれ",
+kana:"だれ",
+hanviet:"",
+vi:"Ai",
+en:"Who",
+category:"Minna 1",
+
+example:`
+<ruby>あの人<rt>あのひと</rt></ruby>は
+<span class="target-word">だれ</span>ですか。
+`,
+example_read:"あのひとは だれですか。",
+example_vi:"Người đó là ai?",
+example_en:"Who is that person?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:19,
+kanji:"どなた",
+kana:"どなた",
+hanviet:"",
+vi:"Vị nào, ai (lịch sự)",
+en:"Who (polite)",
+category:"Minna 1",
+
+example:`
+<ruby>あの方<rt>あのかた</rt></ruby>は
+<span class="target-word">どなた</span>ですか。
+`,
+example_read:"あのかたは どなたですか。",
+example_vi:"Vị kia là ai?",
+example_en:"Who is that person?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:20,
+
+kanji:"～歳",
+kana:"～さい",
+hanviet:"TUẾ",
+vi:"~ tuổi",
+en:"~ years old",
+category:"Minna 1",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は25
+<span class="target-word"><ruby>歳<rt>さい</rt></ruby></span>です。
+`,
+example_read:"わたしは にじゅうごさいです。",
+example_vi:"Tôi 25 tuổi.",
+example_en:"I am 25 years old."
+},
+
+{
+level:"N5",
+lesson:1,
+id:21,
+
+kanji:"何歳",
+kana:"なんさい",
+hanviet:"HÀ TUẾ",
+vi:"Mấy tuổi",
+en:"How old",
+category:"Minna 1",
+
+example:`
+<ruby>あなた</ruby>は
+<span class="target-word"><ruby>何歳<rt>なんさい</rt></ruby></span>ですか。
+`,
+example_read:"あなたは なんさいですか。",
+example_vi:"Bạn bao nhiêu tuổi?",
+example_en:"How old are you?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:22,
+
+kanji:"",
+kana:"おいくつ",
+hanviet:"",
+vi:"Bao nhiêu tuổi (lịch sự)",
+en:"How old (polite)",
+category:"Minna 1",
+
+example:`
+<span class="target-word">おいくつ</span>ですか。
+`,
+example_read:"おいくつですか。",
+example_vi:"Anh/chị bao nhiêu tuổi?",
+example_en:"How old are you?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:23,
+
+kanji:"",
+kana:"はい",
+hanviet:"",
+vi:"Vâng, dạ",
+en:"Yes",
+category:"Minna 1",
+
+example:`
+<span class="target-word">はい</span>、そうです。
+`,
+example_read:"はい、そうです。",
+example_vi:"Vâng, đúng vậy.",
+example_en:"Yes, that's right."
+},
+
+{
+level:"N5",
+lesson:1,
+id:24,
+
+kanji:"",
+kana:"いいえ",
+hanviet:"",
+vi:"Không",
+en:"No",
+category:"Minna 1",
+
+example:`
+<span class="target-word">いいえ</span>、
+<ruby>違<rt>ちが</rt></ruby>います。
+`,
+example_read:"いいえ、ちがいます。",
+example_vi:"Không, không phải vậy.",
+example_en:"No, that's not correct."
+},
+
+{
+level:"N5",
+lesson:1,
+id:25,
+
+kanji:"初めまして",
+kana:"はじめまして",
+hanviet:"SƠ",
+vi:"Rất hân hạnh được gặp",
+en:"Nice to meet you",
+category:"Minna 1",
+
+example:`
+<span class="target-word"><ruby>初<rt>はじ</rt></ruby>めまして</span>。
+<ruby>田中<rt>たなか</rt></ruby>です。
+`,
+example_read:"はじめまして。たなかです。",
+example_vi:"Rất hân hạnh được gặp. Tôi là Tanaka.",
+example_en:"Nice to meet you. I'm Tanaka."
+},
+
+{
+level:"N5",
+lesson:1,
+id:26,
+
+kanji:"来る",
+kana:"きます",
+hanviet:"LAI",
+vi:"Đến",
+en:"Come",
+category:"Minna 1",
+
+example:`
+ベトナムから
+<span class="target-word"><ruby>来<rt>き</rt></ruby>ました</span>。
+`,
+example_read:"ベトナムから きました。",
+example_vi:"Tôi đến từ Việt Nam.",
+example_en:"I came from Vietnam."
+},
+
+{
+level:"N5",
+lesson:1,
+id:27,
+
+kanji:"どうぞよろしくお願いします",
+kana:"どうぞよろしくおねがいします",
+hanviet:"NGUYỆN",
+vi:"Rất mong được giúp đỡ",
+en:"Nice to meet you / Please treat me well",
+category:"Minna 1",
+
+example: `
+<span class="target-word">
+どうぞよろしく<ruby>お願<rt>おねが</rt></ruby>いします
+</span>
+`,
+example_read:"どうぞよろしくおねがいします。",
+example_vi:"Rất mong được giúp đỡ.",
+example_en:"Nice to meet you."
+},
+
+{
+level:"N5",
+lesson:1,
+id:28,
+
+kanji:"失礼ですが",
+kana:"しつれいですが",
+hanviet:"THẤT LỄ",
+vi:"Xin lỗi nhưng...",
+en:"Excuse me but...",
+category:"Minna 1",
+
+example:`
+<span class="target-word">
+<ruby>失礼<rt>しつれい</rt></ruby>ですが
+</span>、
+お<ruby>名前<rt>なまえ</rt></ruby>は？
+`,
+example_read:"しつれいですが、おなまえは？",
+example_vi:"Xin lỗi, tên anh/chị là gì?",
+example_en:"Excuse me, what's your name?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:29,
+
+kanji:"お名前",
+kana:"おなまえ",
+hanviet:"DANH TIỀN",
+vi:"Tên",
+en:"Name",
+category:"Minna 1",
+
+example:`
+<span class="target-word">お<ruby>名前<rt>なまえ</rt></ruby></span>は
+<ruby>何<rt>なん</rt></ruby>ですか。
+`,
+example_read:"おなまえは なんですか。",
+example_vi:"Tên bạn là gì?",
+example_en:"What is your name?"
+},
+
+{
+level:"N5",
+lesson:1,
+id:30,
+
+kanji:"こちらは～さんです",
+kana:"こちらは～さんです",
+hanviet:"",
+vi:"Đây là anh/chị ~",
+en:"This is Mr./Ms. ~",
+category:"Minna 1",
+
+example:`
+<span class="target-word">
+こちらは
+<ruby>田中<rt>たなか</rt></ruby>さんです
+</span>。
+`,
+example_read:"こちらは たなかさんです。",
+example_vi:"Đây là anh Tanaka.",
+example_en:"This is Mr. Tanaka."
+},
+
+{
+level:"N5",
+lesson:1,
+id:31,
+
+kanji:"",
+kana:"アメリカ",
+hanviet:"",
+vi:"Mỹ",
+en:"America",
+category:"Quốc gia",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word">アメリカ</span><ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"わたしは アメリカじんです。",
+example_vi:"Tôi là người Mỹ.",
+example_en:"I am American."
+},
+
+{
+level:"N5",
+lesson:1,
+id:32,
+
+kanji:"",
+kana:"イギリス",
+hanviet:"",
+vi:"Anh",
+en:"United Kingdom",
+category:"Quốc gia",
+
+example:`
+<span class="target-word">イギリス</span>から
+<ruby>来<rt>き</rt></ruby>ました。
+`,
+example_read:"イギリスから きました。",
+example_vi:"Tôi đến từ Anh.",
+example_en:"I came from the UK."
+},
+
+{
+level:"N5",
+lesson:1,
+id:33,
+
+kanji:"",
+kana:"インド",
+hanviet:"",
+vi:"Ấn Độ",
+en:"India",
+category:"Quốc gia",
+
+example:`
+<ruby>彼<rt>かれ</rt></ruby>は
+<span class="target-word">インド</span><ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"かれは インドじんです。",
+example_vi:"Anh ấy là người Ấn Độ.",
+example_en:"He is Indian."
+},
+
+{
+level:"N5",
+lesson:1,
+id:34,
+
+kanji:"",
+kana:"インドネシア",
+hanviet:"",
+vi:"Indonesia",
+en:"Indonesia",
+category:"Quốc gia",
+
+example:`
+<ruby>彼女<rt>かのじょ</rt></ruby>は
+<span class="target-word">インドネシア</span><ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"かのじょは インドネシアじんです。",
+example_vi:"Cô ấy là người Indonesia.",
+example_en:"She is Indonesian."
+},
+
+{
+level:"N5",
+lesson:1,
+id:35,
+
+kanji:"韓国",
+kana:"かんこく",
+hanviet:"HÀN QUỐC",
+vi:"Hàn Quốc",
+en:"Korea",
+category:"Quốc gia",
+
+example:`
+<span class="target-word"><ruby>韓国<rt>かんこく</rt></ruby></span>から
+<ruby>来<rt>き</rt></ruby>ました。
+`,
+example_read:"かんこくから きました。",
+example_vi:"Tôi đến từ Hàn Quốc.",
+example_en:"I came from Korea."
+},
+
+{
+level:"N5",
+lesson:1,
+id:36,
+
+kanji:"",
+kana:"タイ",
+hanviet:"",
+vi:"Thái Lan",
+en:"Thailand",
+category:"Quốc gia",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word">タイ</span><ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"わたしは タイじんです。",
+example_vi:"Tôi là người Thái.",
+example_en:"I am Thai."
+},
+
+{
+level:"N5",
+lesson:1,
+id:37,
+
+kanji:"中国",
+kana:"ちゅうごく",
+hanviet:"TRUNG QUỐC",
+vi:"Trung Quốc",
+en:"China",
+category:"Quốc gia",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word"><ruby>中国<rt>ちゅうごく</rt></ruby></span>
+<ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"わたしは ちゅうごくじんです。",
+example_vi:"Tôi là người Trung Quốc.",
+example_en:"I am Chinese."
+},
+
+{
+level:"N5",
+lesson:1,
+id:38,
+
+kanji:"",
+kana:"ドイツ",
+hanviet:"",
+vi:"Đức",
+en:"Germany",
+category:"Quốc gia",
+
+example:`
+<span class="target-word">ドイツ</span>から
+<ruby>来<rt>き</rt></ruby>ました。
+`,
+example_read:"ドイツから きました。",
+example_vi:"Tôi đến từ Đức.",
+example_en:"I came from Germany."
+},
+
+{
+level:"N5",
+lesson:1,
+id:39,
+
+kanji:"日本",
+kana:"にほん",
+hanviet:"NHẬT BẢN",
+vi:"Nhật Bản",
+en:"Japan",
+category:"Quốc gia",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word">
+<ruby>日本<rt>にほん</rt></ruby>
+</span><ruby>人<rt>じん</rt></ruby>です。
+`,
+example_read:"わたしは にほんじんです。",
+example_vi:"Tôi là người Nhật.",
+example_en:"I am Japanese."
+},
+
+{
+level:"N5",
+lesson:1,
+id:40,
+
+kanji:"",
+kana:"ブラジル",
+hanviet:"",
+vi:"Brazil",
+en:"Brazil",
+category:"Quốc gia",
+
+example:`
+<span class="target-word">ブラジル</span>から
+<ruby>来<rt>き</rt></ruby>ました。
+`,
+example_read:"ブラジルから きました。",
+example_vi:"Tôi đến từ Brazil.",
+example_en:"I came from Brazil."
+},
+
+{
+level:"N5",
+lesson:1,
+id:41,
+
+kanji:"",
+kana:"IMC",
+hanviet:"",
+vi:"Công ty IMC",
+en:"IMC Company",
+category:"Tên riêng",
+
+example:`
+<ruby>私<rt>わたし</rt></ruby>は
+<span class="target-word">IMC</span>の
+<ruby>社員<rt>しゃいん</rt></ruby>です。
+`,
+example_read:"わたしは IMCの しゃいんです。",
+example_vi:"Tôi là nhân viên công ty IMC.",
+example_en:"I am an employee of IMC."
+},
+
+{
+level:"N5",
+lesson:1,
+id:42,
+
+kanji:"",
+kana:"パワーでんき",
+hanviet:"ĐIỆN LỰC",
+vi:"Công ty Power Denki",
+en:"Power Denki Company",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">パワーでんき</span>の
+<ruby>社員<rt>しゃいん</rt></ruby>です。
+`,
+example_read:"パワーでんきの しゃいんです。",
+example_vi:"Tôi là nhân viên công ty Power Denki.",
+example_en:"I am an employee of Power Denki."
+},
+
+{
+level:"N5",
+lesson:1,
+id:43,
+
+kanji:"",
+kana:"ブラジルエアー",
+hanviet:"",
+vi:"Hãng hàng không Brazil Air",
+en:"Brazil Air",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">ブラジルエアー</span>の
+<ruby>社員<rt>しゃいん</rt></ruby>です。
+`,
+example_read:"ブラジルエアーの しゃいんです。",
+example_vi:"Tôi là nhân viên hãng Brazil Air.",
+example_en:"I am an employee of Brazil Air."
+},
+
+{
+level:"N5",
+lesson:1,
+id:44,
+
+kanji:"",
+kana:"AKC",
+hanviet:"",
+vi:"Tổ chức AKC",
+en:"AKC Organization",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">AKC</span>の
+<ruby>研究者<rt>けんきゅうしゃ</rt></ruby>です。
+`,
+example_read:"AKCの けんきゅうしゃです。",
+example_vi:"Tôi là nhà nghiên cứu của AKC.",
+example_en:"I am a researcher at AKC."
+},
+
+{
+level:"N5",
+lesson:1,
+id:45,
+
+kanji:"神戸病院",
+kana:"こうべびょういん",
+hanviet:"THẦN HỘ BỆNH VIỆN",
+vi:"Bệnh viện Kobe",
+en:"Kobe Hospital",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">
+<ruby>神戸病院<rt>こうべびょういん</rt></ruby></span>の<ruby>医者<rt>いしゃ</rt></ruby>です。
+`,
+example_read:"こうべびょういんの いしゃです。",
+example_vi:"Tôi là bác sĩ của bệnh viện Kobe.",
+example_en:"I am a doctor at Kobe Hospital."
+},
+
+{
+level:"N5",
+lesson:1,
+id:46,
+
+kanji:"さくら大学",
+kana:"さくらだいがく",
+hanviet:"ĐẠI HỌC SAKURA",
+vi:"Đại học Sakura",
+en:"Sakura University",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">
+<ruby>さくら大学<rt>さくらだいがく</rt></ruby></span>の<ruby>学生<rt>がくせい</rt></ruby>です。
+`,
+example_read:"さくらだいがくの がくせいです。",
+example_vi:"Tôi là sinh viên Đại học Sakura.",
+example_en:"I am a student at Sakura University."
+},
+
+{
+level:"N5",
+lesson:1,
+id:47,
+
+kanji:"富士大学",
+kana:"ふじだいがく",
+hanviet:"PHÚ SĨ ĐẠI HỌC",
+vi:"Đại học Phú Sĩ",
+en:"Fuji University",
+category:"Tên riêng",
+
+example:`
+<span class="target-word">
+<ruby>富士大学<rt>ふじだいがく</rt></ruby></span>の<ruby>先生<rt>せんせい</rt></ruby>です。
+`,
+example_read:"ふじだいがくの せんせいです。",
+example_vi:"Tôi là giáo viên của Đại học Phú Sĩ.",
+example_en:"I am a teacher at Fuji University."
+}
+];
